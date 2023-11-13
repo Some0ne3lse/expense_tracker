0911d9e065853e833afe587ff614b94cf74cc40b
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2023-11-13 23:51:07 +0000

    change to _titleController

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,10 +8,13 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
+  final _titleController = TextEditingController();
+  // remember to always add dispose method to TextEditingController
 
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
   }
 
   @override
@@ -21,7 +24,7 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(
               label: Text('Title'),
@@ -31,7 +34,7 @@ class _NewExpenseState extends State<NewExpense> {
             children: [
               ElevatedButton(
                   onPressed: () {
-                    print(_enteredTitle);
+                    print(_titleController.text);
                   },
                   child: const Text('Save Expense'))
             ],
